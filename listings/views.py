405d6a13c187567f19cd86e63d1daba83ad2c678
nivats <nivats@microsoft.com>
2019-09-07 23:29:30 +0530

    Pagination done

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import EmptyPage, Paginator
+
+from .models import Listing
 
 def index(request):
-    return render(request,'listings/listings.html')
+    listings = Listing.objects.all()
 
-def listing(request):
+    paginator = Paginator(listings, 6)
+
+    page = request.GET.get('page')
+
+    paged_listings = paginator.get_page(page)
+
+    context = {
+        'listings': paged_listings
+    }
+    return render(request,'listings/listings.html', context)
+
+def listing(request,listing_id):
     return render(request,'listings/listing.html')
 
 def search(request):
