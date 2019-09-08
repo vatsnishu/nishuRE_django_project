dd98671ac16fbe4e3d645cd517cb183fdcb0ca75
nivats <nivats@microsoft.com>
2019-09-08 17:00:20 +0530

    Login completed

--- accounts/views.py
+++ accounts/views.py
@@ -43,9 +43,17 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        print('submitted_login')
         #login user
-        return redirect('login')
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
