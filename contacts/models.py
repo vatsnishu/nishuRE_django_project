4d6d122e7f7c386428d63bd26190f80695746d5a
nivats <nivats@microsoft.com>
2019-09-08 19:38:21 +0530

    inquiry submission complete

--- contacts/models.py
+++ contacts/models.py
@@ -12,5 +12,6 @@ class Contact(models.Model):
     message = models.TextField(blank=True)
     contact_date = models.DateTimeField(default=datetime.now, blank=True)
     user_id = models.IntegerField(blank=True)
+    realtor_email = models.CharField(max_length=100, blank=True)
     def __str__(self):
         return self.name
--- contacts/views.py
+++ contacts/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from django.contrib import messages
+
+from .models import Contact
 
 def contact(request):
-    return
+    if request.method == 'POST':
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
+
+        contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id, realtor_email=realtor_email)
+
+        contact.save()
+
+        messages.success(request, message= 'Your request is submitted, A realtor will be back to you soon')
+        return redirect('/listings/'+listing_id)
